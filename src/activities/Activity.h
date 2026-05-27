dd4d311fef21f74e6cefbfd21d1aed5a9596b5cc
ShallowGreen123 <2608653986@qq.com>
2026-05-27 15:38:17 +0800

    Optimize the refresh method

--- src/activities/Activity.h
+++ src/activities/Activity.h
@@ -43,7 +43,7 @@ class Activity {
   virtual bool skipLoopDelay() { return false; }
   virtual bool preventAutoSleep() { return false; }
   virtual bool isReaderActivity() const { return false; }
-  virtual HalDisplay::RefreshMode defaultRefreshMode() const { return HalDisplay::FULL_REFRESH; }
+  virtual HalDisplay::RefreshMode defaultRefreshMode() const { return HalDisplay::FAST_REFRESH; }
 
   // Start a new activity without destroying the current one
   // Note: requestUpdate() will be invoked automatically once resultHandler finishes
