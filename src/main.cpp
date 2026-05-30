71e7717cde29fb1693ea222e9e2242f64ea83fdb
ShallowGreen123 <2608653986@qq.com>
2026-05-30 09:03:03 +0800

    Improve the boot-up speed

--- src/main.cpp
+++ src/main.cpp
@@ -320,26 +320,16 @@ void setup() {
 
   setupDisplayAndFonts();
 
-  activityManager.goToBoot();
-
   APP_STATE.loadFromFile();
   RECENT_BOOKS.loadFromFile();
 
   if (HalSystem::isRebootFromPanic()) {
     // If we rebooted from a panic, go to crash report screen to show the panic info
     activityManager.goToCrashReport();
-  } else if (APP_STATE.openEpubPath.empty() || !APP_STATE.lastSleepFromReader ||
-             mappedInputManager.isPressed(MappedInputManager::Button::Back) || APP_STATE.readerActivityLoadCount > 0) {
-    // Boot to home screen if no book is open, last sleep was not from reader, back button is held, or reader activity
-    // crashed (indicated by readerActivityLoadCount > 0)
-    activityManager.goHome();
   } else {
-    // Clear app state to avoid getting into a boot loop if the epub doesn't load
-    const auto path = APP_STATE.openEpubPath;
-    APP_STATE.openEpubPath = "";
-    APP_STATE.readerActivityLoadCount++;
-    APP_STATE.saveToFile();
-    activityManager.goToReader(path);
+    // Normal startup always lands on the main home page so boot is faster and
+    // we don't flash intermediate screens or auto-resume into reader content.
+    activityManager.goHome();
   }
 
   // Ensure we're not still holding the power button before leaving setup
